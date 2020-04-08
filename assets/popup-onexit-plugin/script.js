07453e3d7fd79866e96d1ac55463707549ff5e6a
chokweb <interactivecreativedesigner@gmail.com>
2020-04-08 15:03:45 -0700

    create github pages

---
+++ assets/popup-onexit-plugin/script.js
@@ -0,0 +1,2 @@
+
+(function(a){a(document).ready(function(){if(!a("html").hasClass("is-builder")){var b=!0;a(document).mouseleave(function(){b&&(a(".mbr-popup[data-on-exit]").each(function(b,c){a(c).modal("show")}),b=!1)})}})})(jQuery);
